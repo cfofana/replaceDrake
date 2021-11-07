bb7bcceaa35491e916509c53f58c0e9e04b3c587
Ceesay Fofana <cfofana51@muleriders.saumag.edu>
2021-11-07 12:22:58 -0500

    add boilerplate files and code, use replaceAll, getElementsByTagName to replace string

---
+++ script.js
@@ -0,0 +1,35 @@
+var regex = /Drake/g;
+var newTxt = document.getElementsByTagName('html')[0].innerText.replaceAll(regex, 'Male Duck');
+console.log(newTxt);
+document.getElementsByTagName('html')[0].innerText = newTxt;
+
+
+
+
+/*
+var text = "In short, everyone likes Drake. Drake’s biggest success has been to Drake’s success later o";
+
+//var where = text.search('Drake');
+//console.log(where);
+//const items = [...text.matchAll('/Drake/i')];
+//console.log(items[0]);
+function replaceDrake(){
+    where = text.search("/Drake/i");
+    console.log(where); 
+    return where;
+}
+
+console.log((document.getElementsByTagName('html')[0].innerHTML).indexOf('/Drake'));
+
+//document.getElementsByTagName('h3')[0].innerText = "fiohdsuyfoei";
+
+//var txtOfDoc = document.getElementsByTagName('html')[0].innerText;
+//console.log(txtOfDoc);
+
+//var arrOfOccurrences = txtOfDoc.match(regex);
+//console.log(arrOfOccurrences);
+
+var arrOfOccurrences = text.match(regex);
+console.log(arrOfOccurrences);
+console.log(text.replaceAll(regex, 'Male Duck'));
+*/
